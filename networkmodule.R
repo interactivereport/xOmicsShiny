8021bbeeeee875c5b94a6a57f9f8f66abe73abb6
Benbo Gao <48807500+bbgao@users.noreply.github.com>
2024-02-14 11:05:50 -0500

    Update networkmodule.R
    change to "DS_names()"

--- networkmodule.R
+++ networkmodule.R
@@ -48,7 +48,7 @@ network_server <- function(id) {
 			ns <- session$ns
 			output$loadedprojects <- renderUI({
 			  req(length(working_project()) > 0)
-			  radioButtons(ns("current_dataset"), label = "Change Working Dataset", choices=names(DataInSets), inline = F, selected=working_project())
+			  radioButtons(ns("current_dataset"), label = "Change Working Dataset", choices=DS_names(), inline = F, selected=working_project())
 			})
 			
 			observeEvent(input$current_dataset, {
