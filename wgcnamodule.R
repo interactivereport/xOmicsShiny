26ecf8a461319c967ce26838e4572d90308938cc
Tinchi Lin <tinchi.lin@biogen.com>
2024-03-01 16:57:19 -0500

    WGCNA: add a progress bar

--- wgcnamodule.R
+++ wgcnamodule.R
@@ -58,154 +58,156 @@ wgcna_server <- function(id) {
 			# otherwise, whenever an input change, WGCNAReactive will be re-calculated
 			# and its re-calculation could take a long time.
 			WGCNAReactive <- eventReactive(input$plotwgcna, {
-			  req(length(working_project()) > 0)
-			  req(DataInSets[[working_project()]]$data_wide)
-			  req(DataInSets[[working_project()]]$ProjectID)
-			  req(DataInSets[[working_project()]]$ProteinGeneName)
-			  ProjectID <- DataInSets[[working_project()]]$ProjectID
-			  
-			  data_wide = DataInSets[[working_project()]]$data_wide
-			  ProteinGeneName  = DataInSets[[working_project()]]$ProteinGeneName
-			  
-			  wgcnafile <- paste("wgcnadata/", ProjectID, ".RData", sep = "")
-			  if (file.exists(wgcnafile)) {
-			    load(wgcnafile)
-			  } else {
+			  withProgress(message = "Running WGCNA", detail = 'This may take a while...', value = 0.2, {
 			    
-			    # Top number of genes
-			    topNum <- as.numeric(input$WGCNAtopNum)
-			    # Gene Label
-			    gene_label <- input$WGCNAgenelable
-			    
-			    dataExpr <- data_wide %>%
-			      na.omit()
-			    gene.names=rownames(dataExpr)
-			    dataExpr <- rownames_to_column(dataExpr, var = "UniqueID")
-			    dataExpr <- dplyr::left_join(dataExpr, ProteinGeneName)
-			    # row names of dataExpr should be unique; allowing for other non-unique
-			    # vector would cause problems
-			    #dataExpr$labelgeneid = dataExpr[,match(gene_label,colnames(dataExpr))]
-			    dataExpr <- dataExpr %>%
-			      dplyr::select(-c(id, Gene.Name, Protein.ID))
-			    
-			    # row names of dataExpr should be unique; allowing for other non-unique
-			    dataExpr <- column_to_rownames(dataExpr, var = "UniqueID")
-			    dataExpr[is.na(dataExpr) | dataExpr=="Inf"] = NA
-			    
-			    SubGeneNames=gene.names[1L:topNum]
-			    
-			    # Ensure all columns are numeric before transposing; otherwise cell values may
-			    # become character, causing problems in WGCNA::blockwiseModules, as happened to
-			    # the Mouse_microglia_RNA-Seq data
-			    dataExpr <-  dataExpr %>%
-			      dplyr::select(tidyselect::where(is.numeric))
-			    
-			    dataExpr = as.data.frame(t(dataExpr))
-			    dataExpr= dataExpr[,1L:topNum]
-			    
-			    WGCNA::allowWGCNAThreads()
-			    ALLOW_WGCNA_THREADS=8L
-			    enableWGCNAThreads()
-			    
-			    # Choose a set of soft-thresholding powers
-			    powers <- c(c(1L:10L), seq(from = 12L, to = 20L, by = 2L))
-			    r2_cutoff <- input$wgcna_rcut
-			    
-			    cor <- WGCNA::cor
-			    sft <- WGCNA::pickSoftThreshold(dataExpr, dataIsExpr = TRUE, powerVector = powers,	corFnc = cor, corOptions = list(use = 'p'),	networkType = "unsigned")
-			    
-			    # Generating adjacency and TOM similarity matrices based on the selected softpower
-			    picked_power <- softPower <- sft$powerEstimate
-			    
-			    ##calclute the adjacency matrix
-			    #adj= WGCNA::adjacency(dataExpr,type = "unsigned", power = softPower)
-			    #
-			    ##turn adjacency matrix into topological overlap to minimize the effects of noise and spurious associations
-			    #TOM=WGCNA::TOMsimilarityFromExpr(dataExpr,networkType = "unsigned", TOMType = "unsigned", power = softPower)
-			    #
-			    #colnames(TOM) = rownames(TOM) = SubGeneNames
-			    #dissTOM = 1 - TOM
-			    #
-			    ##Module Detection
-			    ##hierarchical clustering of the genes based on the TOM dissimilarity measure
-			    #geneTree = flashClust::flashClust(as.dist(dissTOM),method="average")
-			    #
-			    ## #plot the resulting clustering tree (dendrogram)
-			    ## plot(geneTree, xlab="", sub="",cex=0.35)
-			    ##
-			    #
-			    ## Set the minimum module size
-			    #minModuleSize = 20;
-			    #
-			    ## Module identification using dynamic tree cut
-			    #dynamicMods = dynamicTreeCut::cutreeDynamic(dendro = geneTree,  method="tree", minClusterSize = minModuleSize);
-			    #
-			    ## #the following command gives the module labels and the size of each module.
-			    ## #Lable 0 is reserved for unassigned genes
-			    ## table(dynamicMods)
-			    #
-			    #dynamicColors = WGCNA::labels2colors(dynamicMods)
-			    ## table(dynamicColors)
-			    ##
-			    ## plotDendroAndColors(geneTree, dynamicColors, "Dynamic Tree Cut",
-			    ##                     dendroLabels = FALSE, hang = 0.03, addGuide = TRUE,
-			    ##                     guideHang = 0.05, main = "Gene dendrogram and module colors")
-			    #
-			    ##discard the unassigned genes, and focus on the rest
-			    #restGenes= (dynamicColors != "grey")
-			    #diss1=1-WGCNA::TOMsimilarityFromExpr(dataExpr[,restGenes], power = softPower)
-			    #
-			    #colnames(diss1) =rownames(diss1) =SubGeneNames[restGenes]
-			    ## hier1=flashClust(as.dist(diss1), method="average" )
-			    ## plotDendroAndColors(hier1, dynamicColors[restGenes], "Dynamic Tree Cut",
-			    ##                     dendroLabels = FALSE, hang = 0.03, addGuide = TRUE,
-			    ##                     guideHang = 0.05, main = "Gene dendrogram and module colors")
-			    #
-			    ##set the diagonal of the dissimilarity to NA
-			    #diag(diss1) = NA;
-			    ##
-			    ## #Visualize the Tom plot. Raise the dissimilarity matrix to the power of 4 to bring out the module structure
-			    ## sizeGrWindow(7,7)
-			    ## TOMplot(diss1, hier1) #, as.character(dynamicColors[restGenes]))
-			    #
-			    ## plot heatmap using plotly
-			    #plotly_heatmap <- plot_ly(z = diss1, type = "heatmap", colors = "YlOrRd")
-			    ## heatmap <- d3heatmap(nba_players, scale = "column", color = "YlOrRd")
-			    ## heat <- heatmap(diss1)
-			    #
-			    
-			    temp_cor <- cor
-			    cor <- WGCNA::cor         # Force it to use WGCNA cor function (fix a namespace conflict issue)
-			    netwk <- blockwiseModules(dataExpr,                # <= input here
-			                              
-			                              # == Adjacency Function ==
-			                              power = picked_power,                # <= power here
-			                              networkType = "signed",
-			                              
-			                              # == Tree and Block Options ==
-			                              deepSplit = 2L,
-			                              pamRespectsDendro = F,
-			                              # detectCutHeight = 0.75,
-			                              minModuleSize = 30, #input$minModuleSize, #30,
-			                              # set block size to be number of genes, so that all
-			                              # genes will be analyzed in a single block
-			                              maxBlockSize = input$WGCNAtopNum,#4000,
-			                              
-			                              # == Module Adjustments ==
-			                              reassignThreshold = 0,
-			                              mergeCutHeight = input$mergeCutHeight,#,0.25,
-			                              
-			                              # == TOM == Archive the run results in TOM file (saves time)
-			                              saveTOMs = F,
-			                              saveTOMFileBase = "ER",
-			                              
-			                              # == Output Options
-			                              numericLabels = T,
-			                              verbose = 3L)
-			    cor <- temp_cor
-			  }
-			  netwk
-			  
+  			  req(length(working_project()) > 0)
+  			  req(DataInSets[[working_project()]]$data_wide)
+  			  req(DataInSets[[working_project()]]$ProjectID)
+  			  req(DataInSets[[working_project()]]$ProteinGeneName)
+  			  ProjectID <- DataInSets[[working_project()]]$ProjectID
+  			  
+  			  data_wide = DataInSets[[working_project()]]$data_wide
+  			  ProteinGeneName  = DataInSets[[working_project()]]$ProteinGeneName
+  			  
+  			  wgcnafile <- paste("wgcnadata/", ProjectID, ".RData", sep = "")
+  			  if (file.exists(wgcnafile)) {
+  			    load(wgcnafile)
+  			  } else {
+  			    
+  			    # Top number of genes
+  			    topNum <- as.numeric(input$WGCNAtopNum)
+  			    # Gene Label
+  			    gene_label <- input$WGCNAgenelable
+  			    
+  			    dataExpr <- data_wide %>%
+  			      na.omit()
+  			    gene.names=rownames(dataExpr)
+  			    dataExpr <- rownames_to_column(dataExpr, var = "UniqueID")
+  			    dataExpr <- dplyr::left_join(dataExpr, ProteinGeneName)
+  			    # row names of dataExpr should be unique; allowing for other non-unique
+  			    # vector would cause problems
+  			    #dataExpr$labelgeneid = dataExpr[,match(gene_label,colnames(dataExpr))]
+  			    dataExpr <- dataExpr %>%
+  			      dplyr::select(-c(id, Gene.Name, Protein.ID))
+  			    
+  			    # row names of dataExpr should be unique; allowing for other non-unique
+  			    dataExpr <- column_to_rownames(dataExpr, var = "UniqueID")
+  			    dataExpr[is.na(dataExpr) | dataExpr=="Inf"] = NA
+  			    
+  			    SubGeneNames=gene.names[1L:topNum]
+  			    
+  			    # Ensure all columns are numeric before transposing; otherwise cell values may
+  			    # become character, causing problems in WGCNA::blockwiseModules, as happened to
+  			    # the Mouse_microglia_RNA-Seq data
+  			    dataExpr <-  dataExpr %>%
+  			      dplyr::select(tidyselect::where(is.numeric))
+  			    
+  			    dataExpr = as.data.frame(t(dataExpr))
+  			    dataExpr= dataExpr[,1L:topNum]
+  			    
+  			    WGCNA::allowWGCNAThreads()
+  			    ALLOW_WGCNA_THREADS=8L
+  			    enableWGCNAThreads()
+  			    
+  			    # Choose a set of soft-thresholding powers
+  			    powers <- c(c(1L:10L), seq(from = 12L, to = 20L, by = 2L))
+  			    r2_cutoff <- input$wgcna_rcut
+  			    
+  			    cor <- WGCNA::cor
+  			    sft <- WGCNA::pickSoftThreshold(dataExpr, dataIsExpr = TRUE, powerVector = powers,	corFnc = cor, corOptions = list(use = 'p'),	networkType = "unsigned")
+  			    
+  			    # Generating adjacency and TOM similarity matrices based on the selected softpower
+  			    picked_power <- softPower <- sft$powerEstimate
+  			    
+  			    ##calclute the adjacency matrix
+  			    #adj= WGCNA::adjacency(dataExpr,type = "unsigned", power = softPower)
+  			    #
+  			    ##turn adjacency matrix into topological overlap to minimize the effects of noise and spurious associations
+  			    #TOM=WGCNA::TOMsimilarityFromExpr(dataExpr,networkType = "unsigned", TOMType = "unsigned", power = softPower)
+  			    #
+  			    #colnames(TOM) = rownames(TOM) = SubGeneNames
+  			    #dissTOM = 1 - TOM
+  			    #
+  			    ##Module Detection
+  			    ##hierarchical clustering of the genes based on the TOM dissimilarity measure
+  			    #geneTree = flashClust::flashClust(as.dist(dissTOM),method="average")
+  			    #
+  			    ## #plot the resulting clustering tree (dendrogram)
+  			    ## plot(geneTree, xlab="", sub="",cex=0.35)
+  			    ##
+  			    #
+  			    ## Set the minimum module size
+  			    #minModuleSize = 20;
+  			    #
+  			    ## Module identification using dynamic tree cut
+  			    #dynamicMods = dynamicTreeCut::cutreeDynamic(dendro = geneTree,  method="tree", minClusterSize = minModuleSize);
+  			    #
+  			    ## #the following command gives the module labels and the size of each module.
+  			    ## #Lable 0 is reserved for unassigned genes
+  			    ## table(dynamicMods)
+  			    #
+  			    #dynamicColors = WGCNA::labels2colors(dynamicMods)
+  			    ## table(dynamicColors)
+  			    ##
+  			    ## plotDendroAndColors(geneTree, dynamicColors, "Dynamic Tree Cut",
+  			    ##                     dendroLabels = FALSE, hang = 0.03, addGuide = TRUE,
+  			    ##                     guideHang = 0.05, main = "Gene dendrogram and module colors")
+  			    #
+  			    ##discard the unassigned genes, and focus on the rest
+  			    #restGenes= (dynamicColors != "grey")
+  			    #diss1=1-WGCNA::TOMsimilarityFromExpr(dataExpr[,restGenes], power = softPower)
+  			    #
+  			    #colnames(diss1) =rownames(diss1) =SubGeneNames[restGenes]
+  			    ## hier1=flashClust(as.dist(diss1), method="average" )
+  			    ## plotDendroAndColors(hier1, dynamicColors[restGenes], "Dynamic Tree Cut",
+  			    ##                     dendroLabels = FALSE, hang = 0.03, addGuide = TRUE,
+  			    ##                     guideHang = 0.05, main = "Gene dendrogram and module colors")
+  			    #
+  			    ##set the diagonal of the dissimilarity to NA
+  			    #diag(diss1) = NA;
+  			    ##
+  			    ## #Visualize the Tom plot. Raise the dissimilarity matrix to the power of 4 to bring out the module structure
+  			    ## sizeGrWindow(7,7)
+  			    ## TOMplot(diss1, hier1) #, as.character(dynamicColors[restGenes]))
+  			    #
+  			    ## plot heatmap using plotly
+  			    #plotly_heatmap <- plot_ly(z = diss1, type = "heatmap", colors = "YlOrRd")
+  			    ## heatmap <- d3heatmap(nba_players, scale = "column", color = "YlOrRd")
+  			    ## heat <- heatmap(diss1)
+  			    #
+  			    
+  			    temp_cor <- cor
+  			    cor <- WGCNA::cor         # Force it to use WGCNA cor function (fix a namespace conflict issue)
+  			    netwk <- blockwiseModules(dataExpr,                # <= input here
+  			                              
+  			                              # == Adjacency Function ==
+  			                              power = picked_power,                # <= power here
+  			                              networkType = "signed",
+  			                              
+  			                              # == Tree and Block Options ==
+  			                              deepSplit = 2L,
+  			                              pamRespectsDendro = F,
+  			                              # detectCutHeight = 0.75,
+  			                              minModuleSize = 30, #input$minModuleSize, #30,
+  			                              # set block size to be number of genes, so that all
+  			                              # genes will be analyzed in a single block
+  			                              maxBlockSize = input$WGCNAtopNum,#4000,
+  			                              
+  			                              # == Module Adjustments ==
+  			                              reassignThreshold = 0,
+  			                              mergeCutHeight = input$mergeCutHeight,#,0.25,
+  			                              
+  			                              # == TOM == Archive the run results in TOM file (saves time)
+  			                              saveTOMs = F,
+  			                              saveTOMFileBase = "ER",
+  			                              
+  			                              # == Output Options
+  			                              numericLabels = T,
+  			                              verbose = 3L)
+  			    cor <- temp_cor
+  			  }
+  			  netwk
+			  })
 			})
 			
 			# Create UI for potentially multiple dendrograms
